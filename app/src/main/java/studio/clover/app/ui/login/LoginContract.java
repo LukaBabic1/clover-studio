255a818769e7fbc698895f7414ea8aaef7a639ce
Luka <luka.babic@fivemintes.eu>
2017-11-28 12:13:59 +0100

    Adding contracts.

---
+++ app/src/main/java/studio/clover/app/ui/login/LoginContract.java
@@ -0,0 +1,16 @@
+package studio.clover.app.ui.login;
+
+public final class LoginContract {
+
+    private LoginContract() {
+
+    }
+
+    public interface Presenter {
+
+    }
+
+    public interface View {
+
+    }
+}
---
+++ app/src/main/java/studio/clover/app/ui/message/MessageContract.java
@@ -0,0 +1,16 @@
+package studio.clover.app.ui.message;
+
+public final class MessageContract {
+
+    private MessageContract() {
+
+    }
+
+    public interface Presenter {
+
+    }
+
+    public interface View {
+
+    }
+}
